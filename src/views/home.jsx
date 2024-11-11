300d11b4c2c645ee80ee1909e006d47e64c15047
ibrahim <155033257+Boim274@users.noreply.github.com>
2024-11-11 15:11:05 +0700

    first commit

---
+++ src/views/home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <div className="p-1 mb-3 bg-light rounded-3">
+            <div className="container-fluid py-5">
+                <h1 className="display-5 fw-bold">REACT (VITE) + LARAVEL</h1>
+                <p className="col-md-8 fs-4">Belajar Crud dengan React dan Laravel 11</p>
+            </div>
+        </div>
+    );
+}
